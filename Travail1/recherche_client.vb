3d1434ecf7c5a8da5824a36e93b58551332d2f92
Éric Larivière <1736363@cegepba.qc.ca>
2019-04-09 14:40:25 -0400

    form recherche client

---
+++ Travail1/recherche_client.vb
@@ -0,0 +1,10 @@
+﻿Public Class recherche_client
+    Private Sub recherche_client_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnGererReservation_Click(sender As Object, e As EventArgs) Handles btnGererReservation.Click
+        Reservation.ShowDialog()
+
+    End Sub
+End Class
